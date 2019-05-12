d7603773cc61f48693c4654e61b53afeabfcb630
Corsicanu <corsicanu22@gmail.com>
2019-05-12 08:24:59 +0300

    gban: less chat, more action

--- tg_bot/modules/global_bans.py
+++ tg_bot/modules/global_bans.py
@@ -91,7 +91,7 @@ def gban(bot: Bot, update: Update, args: List[str]):
 
         return
 
-    message.reply_text("<i>Grabbing the banhammer and some popcorn</i>", parse_mode=ParseMode.HTML)
+    # message.reply_text("<i>Grabbing the banhammer and some popcorn</i>", parse_mode=ParseMode.HTML)
 
     banner = update.effective_user  # type: Optional[User]
     send_to_list(bot, SUDO_USERS + SUPPORT_USERS,
