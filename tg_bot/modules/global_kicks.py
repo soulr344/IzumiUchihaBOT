836ee36b94b8e86326a8066b6ec2e73891847284
nunopenim <nuno.penim@gmail.com>
2019-08-19 14:30:13 +0100

    disabled start sticker and other changes

--- tg_bot/modules/global_kicks.py
+++ tg_bot/modules/global_kicks.py
@@ -10,6 +10,7 @@ from tg_bot.modules.helper_funcs.extraction import extract_user, extract_user_an
 from tg_bot.modules.helper_funcs.filters import CustomFilters
 from tg_bot.modules.helper_funcs.misc import send_to_list
 from tg_bot.modules.sql.users_sql import get_all_chats
+import tg_bot.modules.sql.global_kicks_sql as sql
 
 GKICK_ERRORS = {
     "Bots can't add new chat members",
@@ -72,9 +73,15 @@ def gkick(bot: Bot, update: Update, args: List[str]):
                                                            user_chat.id), 
                 html=True)
     message.reply_text("Globally kicking user @{}".format(user_chat.username))
+    sql.gkick_user(user_id, user_chat.username, 1)
     for chat in chats:
         try:
-             bot.unban_chat_member(chat.chat_id, user_id)  # Unban_member = kick (and not ban)
+            member = bot.get_chat_member(chat.chat_id, user_id)
+            if member.can_send_messages is False:
+                bot.unban_chat_member(chat.chat_id, user_id)  # Unban_member = kick (and not ban)
+                bot.restrict_chat_member(chat.chat_id, user_id, can_send_messages = False)
+            else:
+                bot.unban_chat_member(chat.chat_id, user_id)
         except BadRequest as excp:
             if excp.message in GKICK_ERRORS:
                 pass
@@ -84,6 +91,77 @@ def gkick(bot: Bot, update: Update, args: List[str]):
         except TelegramError:
             pass
 
+def __user_info__(user_id):
+    times = sql.get_times(user_id)
+    
+    if int(user_id) in SUDO_USERS or int(user_id) in SUPPORT_USERS:
+        text="Globally kicked: <b>No</b> (Immortal)"
+    else:
+        text = "Globally kicked: {}"
+        if times!=0:
+            text = text.format("<b>Yes</b> (Times: {})".format(times))
+        else:
+            text = text.format("<b>No</b>")
+    return text
+
+@run_async
+def gkickset(bot: Bot, update: Update, args: List[str]):
+    message = update.effective_message
+    user_id, value = extract_user_and_text(message, args)
+    try:
+        user_chat = bot.get_chat(user_id)
+    except BadRequest as excp:
+        if excp.message in GKICK_ERRORS:
+            pass
+        else:
+            message.reply_text("GENERIC ERROR: {}".format(excp.message))
+    except TelegramError:
+        pass
+    if not user_id:
+        message.reply_text("You do not seems to be referring to a user")
+        return  
+    if int(user_id) in SUDO_USERS or int(user_id) in SUPPORT_USERS:
+        message.reply_text("SUDOER: Irrelevant")
+        return
+    if int(user_id) == OWNER_ID:
+        message.reply_text("OWNER: Irrelevant")
+        return
+    if user_id == bot.id:
+        message.reply_text("It's me, nigga")
+        return
+      
+    sql.gkick_setvalue(user_id, user_chat.username, int(value))
+    return
+
+def gkickreset(bot: Bot, update: Update, args: List[str]):
+    message = update.effective_message
+    user_id, value = extract_user_and_text(message, args)
+    try:
+        user_chat = bot.get_chat(user_id)
+    except BadRequest as excp:
+        if excp.message in GKICK_ERRORS:
+            pass
+        else:
+            message.reply_text("GENERIC ERROR: {}".format(excp.message))
+    except TelegramError:
+        pass
+    if not user_id:
+        message.reply_text("You do not seems to be referring to a user")
+        return  
+    if int(user_id) in SUDO_USERS or int(user_id) in SUPPORT_USERS:
+        message.reply_text("SUDOER: Irrelevant")
+        return
+    if int(user_id) == OWNER_ID:
+        message.reply_text("OWNER: Irrelevant")
+        return
+    if user_id == bot.id:
+        message.reply_text("It's me, nigga")
+        return
+      
+    sql.gkick_reset(user_id)
+    return
+
+			
 GKICK_HANDLER = CommandHandler("gkick", gkick, pass_args=True,
                               filters=CustomFilters.sudo_filter | CustomFilters.support_filter)
 dispatcher.add_handler(GKICK_HANDLER)
