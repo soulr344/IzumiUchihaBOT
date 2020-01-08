b98d5d1a855fe6e861a0e04de152ec908d877235
Nuno Penim <nuno.penim@gmail.com>
2020-01-08 12:18:25 +0000

    welcome_sql: preparations for new feature

--- tg_bot/modules/sql/welcome_sql.py
+++ tg_bot/modules/sql/welcome_sql.py
@@ -86,12 +86,24 @@ class BannedChat(BASE):
     def __init__(self, chat_id):
         self.chat_id = str(chat_id) #chat_id is int, make sure it is string
 
+class DefenseMode(BASE):
+    __tablename__ = "defense_mode"
+    chat_id = Column(String(14), primary_key=True)
+    status = Column(Boolean, default=False)
+    
+    def __init__(self, chat_id, status):
+        self.chat_id = str(chat_id)
+        self.status = status
+
+
+
 Welcome.__table__.create(checkfirst=True)
 WelcomeButtons.__table__.create(checkfirst=True)
 GoodbyeButtons.__table__.create(checkfirst=True)
 WelcomeMute.__table__.create(checkfirst=True)
 CombotCASStatus.__table__.create(checkfirst=True)
 BannedChat.__table__.create(checkfirst=True)
+DefenseMode.__table__.create(checkfirst=True)
 
 INSERTION_LOCK = threading.RLock()
 WELC_BTN_LOCK = threading.RLock()
@@ -99,6 +111,7 @@ LEAVE_BTN_LOCK = threading.RLock()
 WM_LOCK = threading.RLock()
 CAS_LOCK = threading.RLock()
 BANCHATLOCK = threading.RLock()
+DEFENSE_LOCK = threading.RLock()
 
 BLACKLIST = set()
 
@@ -376,5 +389,23 @@ def unblacklistChat(chat_id):
 
 def isBanned(chat_id):
     return chat_id in BLACKLIST
+
+def getDefenseStatus(chat_id):
+    try:
+        resultObj = SESSION.query(DefenseMode).get(str(chat_id))
+        if resultObj:
+            return resultObj.status
+        return False #default
+    finally:
+        SESSION.close()
+
+def setDefenseStatus(chat_id, status):
+    with DEFENSE_LOCK:
+        prevObj = SESSION.query(DefenseMode).get(str(chat_id))
+        if prevObj:
+            SESSION.delete(prevObj)
+        newObj = DefenseMode(str(chat_id), status)
+        SESSION.add(newObj)
+        SESSION.commit()
 
 __load_blacklisted_chats_list()
