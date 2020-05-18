a4e21bc9a943a7d6876ef750d431679d8856b3ee
Nuno Penim <nuno.penim@gmail.com>
2020-05-18 20:02:19 +0100

    bans: kick now supports multiple users, added exceptions for TG broadcast account, performance fixes, multiple changes

--- tg_bot/modules/bans.py
+++ tg_bot/modules/bans.py
@@ -28,28 +28,20 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
     message = update.effective_message  # type: Optional[Message]
 
     banType = 0
-
     userIds = extract_multiple_users(message, args)
-
     allTrue = True
-
     for id in userIds:
         if id is None or not isinstance(id, int) or id == "":
             allTrue = False
-
     if len(userIds) > 1 and allTrue:
         banType = 1
-
     if banType == 1:
-        userIds = extract_multiple_users(message, args)
-
         if len(userIds) == 0:
             message.reply_text("There was a problem parsing the user IDs")
             return ""
         log = "#BAN MUL\n Admin: " + user.first_name + "\n"
         for id in userIds:
             try:
-
                 integerID = int(id)
                 member = chat.get_member(integerID)
                 if is_user_ban_protected(chat, integerID, member):
@@ -57,6 +49,9 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
                     continue
                 if integerID == bot.id:
                     message.reply_text("I wont ban myself... " + str(integerID) + " is my ID.")
+                    continue
+                if integerID == 777000:
+                    message.reply_text(str(integerID) + " is an account reserved for telegram, I cannot ban it")
                     continue
                 try:
                     chat.kick_member(integerID)
@@ -82,11 +77,9 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
 
     else:
         user_id, reason = extract_user_and_text(message, args)
-
         if not user_id or int(user_id) == 777000:
             message.reply_text("You don't seem to be referring to a user.")
             return ""
-
         try:
             member = chat.get_member(user_id)
         except BadRequest as excp:
@@ -95,15 +88,12 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
                 return ""
             else:
                 raise
-
         if is_user_ban_protected(chat, user_id, member):
             message.reply_text("Can't do that, user is admin..")
             return ""
-
         if user_id == bot.id:
             message.reply_text("hahahahahahaha nice try.. nope")
             return ""
-
         log = "<b>{}:</b>" \
               "\n#BANNED" \
               "\n<b>Admin:</b> {}" \
@@ -113,7 +103,6 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
                                                            member.user.id)
         if reason:
             log += "\n<b>Reason:</b> {}".format(reason)
-
         try:
             chat.kick_member(user_id)
             keyboard = []
@@ -121,7 +110,6 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
             reply = "{} has been banned!".format(mention_html(member.user.id, member.user.first_name))
             message.reply_text(reply, reply_markup=keyboard, parse_mode=ParseMode.HTML)
             return log
-
         except BadRequest as excp:
             if excp.message == "Reply message not found":
                 # Do not reply
@@ -132,9 +120,7 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
                 LOGGER.exception("ERROR banning user %s in chat %s (%s) due to %s", user_id, chat.title, chat.id,
                                  excp.message)
                 message.reply_text("Well damn, I can't ban that user.")
-
         return ""
-
 
 @run_async
 @bot_admin
@@ -228,52 +214,96 @@ def kick(bot: Bot, update: Update, args: List[str]) -> str:
     user = update.effective_user  # type: Optional[User]
     message = update.effective_message  # type: Optional[Message]
 
-    user_id, reason = extract_user_and_text(message, args)
+    banType = 0
+    userIds = extract_multiple_users(message, args)
+    allTrue = True
+    for id in userIds:
+        if id is None or not isinstance(id, int) or id == "":
+            allTrue = False
+    if len(userIds) > 1 and allTrue:
+        banType = 1
 
-    if not user_id or int(user_id)==777000:
-        message.reply_text("You don't seem to be referring to a user.")
-        return ""
-
-    try:
-        member = chat.get_member(user_id)
-    except BadRequest as excp:
-        if excp.message == "User not found":
-            message.reply_text("404 - user not found")
+    if banType == 1:
+        if len(userIds) == 0:
+            message.reply_text("There was a problem parsing the user IDs")
             return ""
-        else:
-            raise
-
-    if is_user_ban_protected(chat, user_id):
-        message.reply_text("I'm not gonna kick an admin... Though I reckon it'd be pretty funny.")
-        return ""
-
-    if user_id == bot.id:
-        message.reply_text("hahahahahahaha nice try.. nope")
-        return ""
-
-    res = chat.unban_member(user_id)  # unban on current user = kick
-    if res:
-        bot.send_sticker(chat.id, BAN_STICKER)  # ban sticker
-        keyboard = []
-        reply = "{} has been kicked!".format(mention_html(member.user.id, member.user.first_name))
-        message.reply_text(reply, reply_markup=keyboard, parse_mode=ParseMode.HTML)
-
-        log = "<b>{}:</b>" \
-              "\n#KICKED" \
-              "\n<b>Admin:</b> {}" \
-              "\n<b>User:</b> {} (<code>{}</code>)".format(html.escape(chat.title),
-                                                           mention_html(user.id, user.first_name),
-                                                           mention_html(member.user.id, member.user.first_name),
-                                                           member.user.id)
-        if reason:
-            log += "\n<b>Reason:</b> {}".format(reason)
-
+        log = "#KICK_MUL\n Admin: " + user.first_name + "\n"
+        for id in userIds:
+            try:
+                integerID = int(id)
+                member = chat.get_member(integerID)
+                if is_user_ban_protected(chat, integerID, member):
+                    message.reply_text("Can't ban " + str(integerID) + ", user is Ban protected.")
+                    continue
+                if integerID == bot.id:
+                    message.reply_text("I wont ban myself... " + str(integerID) + " is my ID.")
+                    continue
+                if integerID == 777000:
+                    message.reply_text(str(integerID) + " is an account reserved for telegram, I cannot ban it")
+                    continue
+                res = chat.unban_member(integerID)
+                if res:
+                    #bot.send_sticker(chat.id, BAN_STICKER)  # ban sticker
+                    keyboard = []
+                    reply = "{} has been kicked!".format(mention_html(member.user.id, member.user.first_name))
+                    message.reply_text(reply, reply_markup=keyboard, parse_mode=ParseMode.HTML)
+                    log += "ID: " + str(member.user.id) + "\n"
+                else:
+                    message.reply_text("Well damn, I can't kick that user.")
+            except ValueError:
+                message.reply_text("Error parsing the ID: " + id + " is not a valid user ID")
+            except BadRequest as excp:
+                if excp.message == "User not found":
+                    message.reply_text("User " + str(integerID) + "has not been found")
+                    continue
+                else:
+                    raise
         return log
-
     else:
-        message.reply_text("Well damn, I can't kick that user.")
+        user_id, reason = extract_user_and_text(message, args)
+        if not user_id or int(user_id) == 777000:
+            message.reply_text("You don't seem to be referring to a user.")
+            return ""
+        try:
+            member = chat.get_member(user_id)
+        except BadRequest as excp:
+            if excp.message == "User not found":
+                message.reply_text("404 - user not found")
+                return ""
+            else:
+                raise
 
-    return ""
+        if is_user_ban_protected(chat, user_id):
+            message.reply_text("I'm not gonna kick an admin... Though I reckon it'd be pretty funny.")
+            return ""
+
+        if user_id == bot.id:
+            message.reply_text("hahahahahahaha nice try.. nope")
+            return ""
+
+        res = chat.unban_member(user_id)  # unban on current user = kick
+        if res:
+            bot.send_sticker(chat.id, BAN_STICKER)  # ban sticker
+            keyboard = []
+            reply = "{} has been kicked!".format(mention_html(member.user.id, member.user.first_name))
+            message.reply_text(reply, reply_markup=keyboard, parse_mode=ParseMode.HTML)
+
+            log = "<b>{}:</b>" \
+                  "\n#KICKED" \
+                  "\n<b>Admin:</b> {}" \
+                  "\n<b>User:</b> {} (<code>{}</code>)".format(html.escape(chat.title),
+                                                               mention_html(user.id, user.first_name),
+                                                               mention_html(member.user.id, member.user.first_name),
+                                                               member.user.id)
+            if reason:
+                log += "\n<b>Reason:</b> {}".format(reason)
+
+            return log
+
+        else:
+            message.reply_text("Well damn, I can't kick that user.")
+
+        return ""
 
 
 @run_async
@@ -364,10 +394,10 @@ This module allows you to do that easily, by exposing some common actions, so ev
  - /banme: bans the user who issued the command.
 
 *Admin only:*
- - /ban <userhandle>: bans a user. (via handle, or reply)
+ - /ban <userhandle1> <userhandle2> <userhandle3>: bans users. (via handle, or reply)
  - /tban <userhandle> x(m/h/d): bans a user for x time. (via handle, or reply). m = minutes, h = hours, d = days.
  - /unban <userhandle>: unbans a user. (via handle, or reply)
- - /kick <userhandle>: kicks a user, (via handle, or reply)
+ - /kick <userhandle1> <userhandle2> <userhandle3>: kicks users. (via handle, or reply)
 
 An example of temporarily banning someone:
 `/tban @username 2h`; this bans a user for 2 hours.
