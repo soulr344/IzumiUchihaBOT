d3620b9cad4ec824134462113957e1c9c33c3dfd
Razvan <corsicanu22@gmail.com>
2020-08-11 23:51:40 +0300

    android: fix magisk maybe

--- tg_bot/modules/android.py
+++ tg_bot/modules/android.py
@@ -16,7 +16,7 @@ DEVICES_DATA = 'https://raw.githubusercontent.com/androidtrackers/certified-andr
 def magisk(bot, update):
     url = 'https://raw.githubusercontent.com/topjohnwu/magisk_files/'
     releases = ""
-    for type, branch in {"Stable":["master/stable","master"], "Beta":["master/beta","master"], "Canary (release)":["canary/release","canary"], "Canary (debug)":["canary/debug","canary"]}.items():
+    for type, branch in {"Stable":["master/stable","master"], "Beta":["master/beta","master"], "Canary":["canary/debug","canary"]}.items():
         data = get(url + branch[0] + '.json').json()
         releases += f'*{type}*: \n' \
                     f'• [Changelog](https://github.com/topjohnwu/magisk_files/blob/{branch[1]}/notes.md)\n' \
